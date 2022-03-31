685d0d95ed96d9571b31d15d638a4284e8ba3641
Aseem <aseem@pangeaemr.com>
2022-03-31 00:13:30 -0500

    2 tests

--- app1/tests.py
+++ app1/tests.py
@@ -4,3 +4,8 @@ from django.test import TestCase
 class TestSomething(TestCase):
     def test1(self):
         self.assertEqual(1, 1)
+
+
+class TestSomething2(TestCase):
+    def test1(self):
+        self.assertEqual(1, 1)
